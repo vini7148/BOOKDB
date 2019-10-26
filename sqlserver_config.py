7682b48cf89f4e9e822ed24a85f15620e2953f88
Vinayak Goswami <vgoswami714841@yahoo.com>
2019-10-27 02:01:38 +0530

    Initial commit

--- sqlserver_config.py
+++ sqlserver_config.py
@@ -1,8 +1,8 @@
 dbConfig = {
 	'driver':'SQL SERVER',
-	'Server':"LAPTOP-6LIV4IK3\SQLEXPRESS",
+	'Server':"",
 	'Database':"mybooks",
-	'username':"LAPTOP-6LIV4IK3\Acer",
-	'password':"9870"
+	'username':"",
+	'password':""
 	
 }
